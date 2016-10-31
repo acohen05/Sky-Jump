4c6e4b5c078907bedc6cdbf0d9208049da05e47d
Kevin Lustgarten <kevinlustgarten@gmail.com>
2016-10-31 14:24:46 -0400

    Add files via upload

---
+++ Crumbler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crumbler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator crumble() {
+		yield return new WaitForSeconds (2.0f);
+		Destroy (gameObject);
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag == "Player")
+			StartCoroutine(crumble());
+	}
+}
